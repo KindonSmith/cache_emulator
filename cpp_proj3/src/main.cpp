a5be6c5952c8b70ee6cb0070553bb3f346e229c9
Kindon-Smith <kindon@bluelim3.com>
2026-04-06 15:38:24 -0700

    Updated input parsing for cache and address list to reside in vector returning functions, rather than void based pass by reference flow

--- cpp_proj3/src/main.cpp
+++ cpp_proj3/src/main.cpp
@@ -7,33 +7,42 @@
 
 using namespace std;
 
-void parseInput(char* inputFileName){
+vector<uint32_t> parseAddresses(char* inputFileName){
   string inputFileRoute = "input/";
   string inputFileString = inputFileRoute + string(inputFileName);
-  //cout <<"InputFileString: " << inputFileString << endl;
   ifstream inputFile(inputFileString);
   uint32_t address{};
+  vector<uint32_t> addressList{};
+
   if (inputFile.is_open()){
     while(inputFile >> std::hex >> address){
-      //cout << "Input file is being read, address: " << address << endl;
+      addressList.push_back(address);
     }
-
+    inputFile.close();
+  }else{
+    cout << "Unable to open input file." << endl;
   }
+  return addressList;
 }
 
 vector<cache_set> buildCache(char* cacheSizeInput, char* blockSizeInput, char* associativityInput){
-  // parse cache and block size from char* to int
+  // cache size - total memory
+  // block size - cache size / block size gives us number of sets
+  // associativity - set size
   int cacheSize = atoi(cacheSizeInput);
   int blockSize = atoi(blockSizeInput);
+  int associativity = atoi(associativityInput);
+
   // if block size is 0, we can't divide by 0, throw exception
   if (blockSize == 0) {
     cout << "Invalid block size of 0." << endl;
     throw runtime_error("Division by zero.");
   }
-  int numSets = cacheSize / blockSize;
 
-  int associativity = atoi(associativityInput);
-
+  int numSets = cacheSize / blockSize / associativity;
+  vector<cache_set> cache(numSets, cache_set(associativity));
+  // cache with correct number of sets, each of associative size  g++ -g main.cpp cache.cpp stats.cpp -o cache_sim
+  return cache;
 }
 
 int main(int argc, char* argv[]) {
@@ -44,4 +53,6 @@ int main(int argc, char* argv[]) {
     return 0;
   }
   vector<cache_set> cache = buildCache(argv[1], argv[2], argv[3]);
+  vector<uint32_t> addressList = parseAddresses(argv[4]);
+
 }
