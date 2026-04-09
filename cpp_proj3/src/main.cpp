4895e460e4c74c1848fa5c644a069ca493b4a540
KindonSmith <kindonsmith@gmail.com>
2026-04-09 16:08:19 -0700

    final n-way cache simulation push. checks on RAII and memory leaks look good. checks on data input look good, though input validation needs improvement

--- cpp_proj3/src/main.cpp
+++ cpp_proj3/src/main.cpp
@@ -26,7 +26,7 @@ vector<uint32_t> parseAddresses(char* inputFileName){
   return addressList;
 }
 
-Cache buildCache(char* cacheSizeInput, char* blockSizeInput, char* associativityInput){
+Cache buildCacheUtility(char* cacheSizeInput, char* blockSizeInput, char* associativityInput){
   // cache size - total memory
   // block size - cache size / block size gives us number of sets
   // associativity - set size
@@ -45,51 +45,9 @@ Cache buildCache(char* cacheSizeInput, char* blockSizeInput, char* associativity
   return Cache(cache_set(associativity), cacheSize, blockSize, associativity);
 }
 
-uint32_t getIndex(uint32_t address, uint32_t bits, uint32_t offset){
-  uint32_t mask{};
-  for(int i = 0; i < bits; i++){
-    mask <<= 1;
-    mask += 1;
-  }
-  return address >> offset & mask;
-}
-uint32_t getTag(uint32_t address, uint32_t bits){
-  return address >> bits;
-}
-
-/*int cacheLookup(Cache &cache, uint32_t address){
-  /*int lruMaxIndex{};
-  uint32_t index = cache.getIndex(address);
-  uint32_t tag = cache.getTag(address);
-  for(int wayIndex = 0; wayIndex < cache.associativity; wayIndex++){
-    if(cache.sets[index].ways[wayIndex].valid){
-      if (cache.sets[index].ways[wayIndex].tag == tag){
-        cache.sets[index].updateLRU(wayIndex);
-        return 1;
-      }
-    }
-
-    int curLRU {cache.sets[index].ways[wayIndex].lru_counter};
-    int maxLRU {cache.sets[index].ways[lruMaxIndex].lru_counter};
-    if(curLRU > maxLRU){
-      lruMaxIndex = wayIndex;
-    }
-  }
-
-  // if we reach here, we've checked all necessary ways and can write
-  // we've also missed, so we return 0
-  cache.sets[index].ways[lruMaxIndex].valid = true;
-  cache.sets[index].ways[lruMaxIndex].tag = tag;
-  cache.sets[index].updateLRU(lruMaxIndex);
-
-  return 0;
-}*/
-
-void checkCache(Cache &cache, vector<uint32_t> addressList, int &hits)
+void checkCache(Cache &cache, const vector<uint32_t> &addressList, int &hits)
 {
   for(int i=0; i<addressList.size(); i++){
-    //uint32_t index = getIndex(addressList[i], cache.indexBits, cache.offsetBits);
-    //uint32_t tag = getTag(addressList[i], cache.offsetBits + cache.indexBits);
     hits += cache.lookUp(addressList[i]);
   }
 }
@@ -103,7 +61,7 @@ int main(int argc, char* argv[]) {
     return 0;
   }
   int hits{};
-  Cache cache = buildCache(argv[1], argv[2], argv[3]);
+  Cache cache = buildCacheUtility(argv[1], argv[2], argv[3]);
   vector<uint32_t> addressList = parseAddresses(argv[4]);
   checkCache(cache, addressList, hits);
   int queries = addressList.size();
