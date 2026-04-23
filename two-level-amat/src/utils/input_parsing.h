0c5c20bf31b75f63dbea3e6f2c91d9f2e18a342e
KindonSmith <kindonsmith@gmail.com>
2026-04-23 16:28:22 -0700

    runtime error when can't open config file
    runtime error when no value is after a parameter or no parameter precedes a value

--- two-level-amat/src/utils/input_parsing.h
+++ two-level-amat/src/utils/input_parsing.h
@@ -32,26 +32,41 @@ ifstream getConfigFile(int numArgs, char** argv){
   return inputFile;
 }
 
-void splitCacheConfigParameterAndValue(string configLine, map<string, string> &pandv){
+void splitCacheConfigParameterAndValue(string configLine, map<string, map<string, string>> &pandv){
   long long positionEquals = configLine.find('=');
   long long positionIdent = configLine.find('_');
+  // need to run checks to make sure our config input is working as expected.
   if (positionEquals == string::npos || positionIdent == string::npos) { 
     throw runtime_error("Malformed config input." + configLine);
   }
-  pandv.insert({configLine.substr(positionIdent, positionEquals), configLine.substr(positionEquals+1)});
-  cout << configLine.substr(positionIdent, positionEquals - 1) << " | " << configLine.substr(positionEquals + 1) << endl;
+  if (positionEquals == 0 || positionEquals == 1){
+    throw runtime_error("Malformed config input: " + configLine + ". \n" + "Please include a parameter before equal sign.");
+  }
+  if (positionEquals == configLine.size() - 1)
+  {
+    throw runtime_error("Malformed config input: " + configLine + ". \n" + "Please include a value after the equal sign.");
+  }
+  pandv[configLine.substr(0, positionIdent)].insert({configLine.substr(positionIdent, positionEquals - 2), configLine.substr(positionEquals+1)});
 }
 
 void parseConfig(ifstream &configFile){
   string configLine{};
-  map<string, string> pandv{};
+  map<string, map<string, string>> pandv{};
   
+  if (!configFile.is_open()) {
+    throw runtime_error("Unable to open configuration file. Please verify config filepath.");
+  }
 
   while(getline(configFile, configLine)){
     splitCacheConfigParameterAndValue(configLine, pandv);
     // do we want a messy switch implementation?
-    // 
+    // what about map<string, map<string, string>> such that L1 and L2 are higher level indexes with values of fields.
   } 
+  for (const auto& cacheConfig : pandv){
+    for (const auto& param : cacheConfig.second){
+      cout << cacheConfig.first << " | " << param.first << " | " << param.second << endl;
+    }
+  }
 
 
 }
