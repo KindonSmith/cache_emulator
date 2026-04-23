0da6447826f9466001d5549c907ed0527f038811
KindonSmith <kindonsmith@gmail.com>
2026-04-23 15:35:29 -0700

    constructor for cache_system: takes vector<Cache>

--- two-level-amat/src/classes/cache_system.h
+++ two-level-amat/src/classes/cache_system.h
@@ -10,12 +10,13 @@ private:
   /* data */
   vector<Cache> cache_list;
 public:
-  Cache_System();
+  Cache_System(vector<Cache> cacheList);
   vector<Cache> get_caches();
 };
 
-Cache_System::Cache_System()
+Cache_System::Cache_System(vector<Cache> cacheList)
 {
+  cache_list = cacheList;
 }
 
 vector<Cache> Cache_System::get_caches(){
