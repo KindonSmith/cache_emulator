77228005dc30f4832393a86670bd546a96aa7d0f
Kindon-Smith <kindonsmith@gmail.com>
2026-04-27 15:14:54 -0700

    lookup_result struct for passing more information to the cache_system - allowing it to handle l1 <-> l2 linking
    nullable cache_block pointer used over reference to support null values in cache hits/misses

--- two-level-amat/src/structs/lookup_result.h
+++ two-level-amat/src/structs/lookup_result.h
@@ -1,10 +1,13 @@
 #ifndef Lookup_Result_H
 #define Lookup_Result_H
 
+#include "cache_structs.h"
+using namespace std;
 
 struct lookup_result{
   bool hit;
   bool writeback_occurred;
+  cache_block* block;
 };
 
 
