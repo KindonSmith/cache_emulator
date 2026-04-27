2e866f68143c8ca10dc5e8ee773aac2312895a7d
Kindon-Smith <kindonsmith@gmail.com>
2026-04-27 14:20:39 -0700

    bounded cache_list in cache_system.h to num_caches, default of 2 for this project

--- two-level-amat/src/classes/cache_system.h
+++ two-level-amat/src/classes/cache_system.h
@@ -9,6 +9,7 @@ class Cache_System
 private:
   /* data */
   vector<Cache> cache_list;
+  int num_caches = 2;
 public:
   Cache_System(vector<Cache> cacheList);
   Cache_System();
@@ -28,7 +29,11 @@ vector<Cache> Cache_System::get_cache_list(){
 
 void Cache_System::insert_cache(cache_config config){
   Cache cache(config);
-  cache_list.push_back(cache);
+  if (cache_list.size() < num_caches){
+      cache_list.push_back(cache);
+  } else {
+    // can't put in more caches 
+  }
 }
 
 
