9318542c817a8d3a73dedbe994dd8f97fec98e58
Kindon-Smith <kindonsmith@gmail.com>
2026-04-29 17:07:12 -0700

    moved method bodies out of cache_system.cpp to resolved linker errors. only declarations should exist in headers

--- two-level-amat/src/classes/cache_system.cpp
+++ two-level-amat/src/classes/cache_system.cpp
@@ -1,9 +1,36 @@
-#include "cache.h"
 #include "cache_system.h"
+
+Cache_System::Cache_System(){}
+
+void Cache_System::insert_cache(cache_config config){
+  Cache cache(config);
+  if (cache_list.find(1) == cache_list.end()){
+      cache_list[1].push_back(cache);
+  } else if (cache_list.find(2) == cache_list.end()){
+      cache_list[2].push_back(cache);
+  } else {
+    cout << "Warning: Only 2 caches supported, but more than 2 configurations found." << endl;
+  }
+}
+
+void Cache_System::set_instructions(vector<Instruction> _instructions){
+  instruction_list = _instructions;
+}
+
+vector<Instruction> Cache_System::get_instructions(){
+  return instruction_list;
+}
 
 lookup_result Cache_System::look_up(uint32_t address, string directive, Cache cache){
   return cache.lookUp(address, directive);
 }
 
 
-void Cache_System::simulate(){}
+void Cache_System::simulate(){
+  auto instructions = get_instructions();
+  for(int i = 0; i < instructions.size(); i++){
+    // for each instruction, we need to lookup in l1. on miss, we lookup l2.
+    // 
+  }
+
+}
--- two-level-amat/src/classes/cache_system.h
+++ two-level-amat/src/classes/cache_system.h
@@ -23,35 +23,4 @@ public:
 };
 
 
-Cache_System::Cache_System(){}
-
-/*vector<Cache> Cache_System::get_cache_list(){
-  return cache_list;
-}*/
-
-void Cache_System::insert_cache(cache_config config){
-  Cache cache(config);
-  // if no key for 1 is found, add key:cache for 1
-  // if no key for 2, add key:cache for 2
-  if (cache_list.find(1) == cache_list.end()){
-      cache_list[1].push_back(cache);
-  } else if (cache_list.find(2) == cache_list.end()){
-      cache_list[2].push_back(cache);
-  } else {
-    // more than 2 caches being pushed. unsupported.
-    cout << "Warning: Only 2 caches supported, but more than 2 configurations found." << endl;
-  }
-}
-
-void Cache_System::set_instructions(vector<Instruction> _instructions){
-  instruction_list = _instructions;
-}
-
-vector<Instruction> Cache_System::get_instructions(){
-  return instruction_list;
-}
-
-
-
-
 #endif
