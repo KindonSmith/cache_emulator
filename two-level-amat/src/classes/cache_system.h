2484b9f33373fb0185141c6c7016897dcaf8ca2d
Kindon-Smith <kindonsmith@gmail.com>
2026-04-30 15:20:10 -0700

    including header file in commit - was not included in .cpp update

--- two-level-amat/src/classes/cache_system.h
+++ two-level-amat/src/classes/cache_system.h
@@ -11,17 +11,19 @@ private:
   int num_caches = 2;
   int memory_access_time{};
   map<int, Cache> cache_list;
-  Stats system_stats;
+  map<int, Stats> cache_stats;
   vector<Instruction> instruction_list;
 public:
   Cache_System();
-  map<int, Cache> get_cache_list();
+  map<int, Cache>& get_cache_list();
   void insert_cache(cache_config config);
-  lookup_result look_up(uint32_t address, string directive, Cache cache);
+  lookup_result look_up(Instruction instruction, Cache& cache);
   void set_instructions(vector<Instruction> _instructions);
   vector<Instruction> get_instruction_list();
   void simulate();
   void set_access_time(int _access_time);
+  void validate_result(lookup_result result, int cache_index, string directive);
+  void insert_instruction(Instruction instruction);
 };
 
 
